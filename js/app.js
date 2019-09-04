224b6a003e72618eef79aadba1e31798d73e6617
nicolasignacio97 <53886403+nicolasignacio97@users.noreply.github.com>
2019-09-04 10:13:00 -0400

    version 1.0.3
    registro

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function ValidarCampos() {
+
+    var NombreTienda = document.getElementById("NombreTienda").Value;
+    var Direccion = document.getElementById("Direccion").Value;
+    var Correo = document.getElementById("Correo").Value;
+    var NombreRepr = document.getElementById("NombreRepr").Value;
+    var Telefono = document.getElementById("Telefono").Value;
+
+    if (NombreTienda == null) {
+
+        alert("Ingrese el nombre de la tienda");
+        console.log("bien");
+
+    }
+
+
+}
